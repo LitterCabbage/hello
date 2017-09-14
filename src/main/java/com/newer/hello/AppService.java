760c1900c92e8a5453647ce4d67e251ec07ac1fd
wtao <wtao@wtao-mac>
2017-09-14 11:29:20 +0800

    实现了 AppService

---
+++ src/main/java/com/newer/hello/AppService.java
@@ -0,0 +1,8 @@
+package com.newer.hello;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AppService {
+
+}
--- src/main/java/com/newer/hello/HelloController.java
+++ src/main/java/com/newer/hello/HelloController.java
@@ -1,8 +1,11 @@
 package com.newer.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HelloController {
 
+	@Autowired
+	AppService appService;
 }
